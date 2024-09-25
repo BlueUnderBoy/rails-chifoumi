3db87fbbe52eb16bd1c39efaab09409ca2e1d573
Jalen Young <120889301+BlueUnderBoy@users.noreply.github.com>
2024-09-25 20:01:18 +0000

    met a jour

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-
+  #get("/", {:controller => "zebra", :action=> "giraffe"})
+  #get("/rock", {:controller => "zebra", :action=> "giraffe"})
+  #get("/paper", {:controller => "zebra", :action=> "giraffe"})
+  #get("/scissors", {:controller => "zebra", :action=> "giraffe"})
+  #can also put ":somethhing" for placeholder in route
 end
